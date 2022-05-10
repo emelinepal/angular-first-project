6667ffaee3b5d82a581c9c672eb946967777a637
emelinepal <emeline43@outlook.fr>
2022-05-10 10:55:21 +0200

    router links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,35 @@ import { AppComponent } from './app.component';
 import { ErrorModule } from './views/error/error/error.module';
 
 const routes: Routes = [
-	{path: '', component: AppComponent},
-	{path: 'about-us',
-	  loadChildren: () =>
-		import('./views/about-us/about-us.module').then(m => m.AboutUsModule)},
-	{path: 'error', component: ErrorModule},
-	{path: '**', redirectTo: 'error'}
-  ];
+	{ path: '', component: AppComponent },
+	{
+		path: 'about-us',
+		loadChildren: () =>
+			import('./views/about-us/about-us.module').then(
+				(m) => m.AboutUsModule
+			),
+	},
+	{
+		path: 'blog',
+		loadChildren: () =>
+			import('./views/blog/blog.module').then((m) => m.BlogModule),
+	},
+	{
+		path: 'news',
+		loadChildren: () =>
+			import('./views/news/news.module').then((m) => m.NewsModule),
+	},
+	{
+		path: 'team',
+		loadChildren: () =>
+			import('./views/team/team.module').then((m) => m.TeamModule),
+	},
+	{ path: 'error', component: ErrorModule },
+	{ path: '**', redirectTo: 'error' },
+];
 
 @NgModule({
-	imports: [
-	  RouterModule.forRoot(routes),
-	],
-    exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
