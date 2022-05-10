371c14f3425def04ea7f631da77c92344defc097
emelinepal <emeline43@outlook.fr>
2022-05-10 14:08:46 +0200

    form team

--- src/app/views/news/news.module.ts
+++ src/app/views/news/news.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NewsComponent } from './news.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+	{
+		path: '', component: NewsComponent
+	}
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NewsComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+	RouterModule.forChild(routes),
   ]
 })
 export class NewsModule { }
