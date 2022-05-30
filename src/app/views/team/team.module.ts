feb2a0ef2c9827926107038f0f755a19a0b0da96
emelinepal <emeline43@outlook.fr>
2022-05-30 15:04:11 +0200

    Revert "menu"
    This reverts commit 35450304597df22186bcbd8d1b9cdf5dbc829f4a.

--- src/app/views/team/team.module.ts
+++ src/app/views/team/team.module.ts
@@ -1,28 +1,29 @@
-import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TeamComponent } from './team.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { MemberComponent } from './component/member/member.component';
-import { NgxDsSecretHouseModule } from 'ngx-ds-secret-house';
 
 const routes: Routes = [
 	{
-		path: '',
-		component: TeamComponent,
-	},
-];
+		path: '', component: TeamComponent
+	}
+]
 
 @NgModule({
-	declarations: [TeamComponent, MemberComponent],
-	schemas: [CUSTOM_ELEMENTS_SCHEMA],
-	imports: [
-		CommonModule,
-		ReactiveFormsModule,
-		RouterModule.forChild(routes),
-		ComponentsModule,
-		NgxDsSecretHouseModule,
-	],
+  declarations: [
+    TeamComponent,
+	MemberComponent,
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+  imports: [
+    CommonModule,
+	ReactiveFormsModule,
+	RouterModule.forChild(routes),
+	ComponentsModule
+  ]
 })
-export class TeamModule {}
+export class TeamModule {
+}
