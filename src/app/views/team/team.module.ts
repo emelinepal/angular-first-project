35450304597df22186bcbd8d1b9cdf5dbc829f4a
emelinepal <emeline43@outlook.fr>
2022-05-30 12:52:45 +0200

    menu

--- src/app/views/team/team.module.ts
+++ src/app/views/team/team.module.ts
@@ -1,29 +1,28 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { TeamComponent } from './team.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { MemberComponent } from './component/member/member.component';
+import { NgxDsSecretHouseModule } from 'ngx-ds-secret-house';
 
 const routes: Routes = [
 	{
-		path: '', component: TeamComponent
-	}
-]
+		path: '',
+		component: TeamComponent,
+	},
+];
 
 @NgModule({
-  declarations: [
-    TeamComponent,
-	MemberComponent,
-  ],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-  imports: [
-    CommonModule,
-	ReactiveFormsModule,
-	RouterModule.forChild(routes),
-	ComponentsModule
-  ]
+	declarations: [TeamComponent, MemberComponent],
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
+	imports: [
+		CommonModule,
+		ReactiveFormsModule,
+		RouterModule.forChild(routes),
+		ComponentsModule,
+		NgxDsSecretHouseModule,
+	],
 })
-export class TeamModule {
-}
+export class TeamModule {}
